bf51bb260769b8f627c6aac71689e8e761ba3fb5
Papassorn Phopong <spriten.papassorn@gmail.com>
2024-02-03 17:29:22 +0700

    update default config

--- config/ksher.php
+++ config/ksher.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'APP_ID' => env('APP_ID'),
-    'ENDPOINT' => env('ENDPOINT'),
-    'MCH_CODE' => env('MCH_CODE')
+    'APP_ID' => env('APP_ID','mch3000'),
+    'ENDPOINT' => env('ENDPOINT','https://85022bb2-e312-4183-822c-aafb3d3af478.mock.pstmn.io/gateway_pay'),
+    'MCH_CODE' => env('MCH_CODE','12345')
 ];
